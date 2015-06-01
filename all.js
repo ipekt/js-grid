b107b8b731b736ac9ddc64647f5d3fbae1c351fe
Ipek Turgul <ipek.turgul@gmail.com>
2015-06-01 21:51:44 +1000

    changing color on mouseover

--- all.js
+++ all.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
   var j = 64;
   for(var i = 0; i < j - 1; i++) {
     $(".container").append('<div class="square"></div>');
-
   };
+  $(".square").mouseover(function() {
+    $(this).removeClass("square").addClass("changeColor");
+  });
  });
