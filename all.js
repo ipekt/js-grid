0bd59cbf56b299a00e77dc6715ef852091a45ba2
Ipek Turgul <ipek.turgul@gmail.com>
2015-06-01 23:24:20 +1000

    completed

--- all.js
+++ all.js
@@ -1,10 +1,33 @@
-// make 16 x 16 grid
+
 $(document).ready(function() {
-  var j = 64;
-  for(var i = 0; i < j - 1; i++) {
-    $(".container").append('<div class="square"></div>');
-  };
-  $(".square").mouseover(function() {
-    $(this).removeClass("square").addClass("changeColor");
+  var grid = 8;
+
+  function bindColourChange(square) {
+    square.mouseover(function() {
+      $(this).removeClass("square").addClass("changeColor");
+    });
+  }
+
+  function makeGrid(grid) {
+    var rows, columns = 0;
+    var row, square, width;
+    for (rows = 0; rows < grid; rows++) {
+      width = (480 - grid - 1) / grid;
+      row = $('<div class="row"' + 'style=height:' + width + 'px;"></div>');
+      for (columns = 0; columns < grid; columns++) {
+        square = $('<div class="square"' + 'style=width:' + width + 'px;' + 'height:' + width + 'px;"></div>');
+        bindColourChange(square);
+        row.append(square);
+      };
+      $('.container').append(row);
+    }
+  }
+
+  $("#changeGrid").on('click', function() {
+    var gridNum = prompt("Please enter grid number you would like", "8");
+    $('.container .row').remove();
+    makeGrid(gridNum);
   });
+
+  makeGrid(grid);
  });
